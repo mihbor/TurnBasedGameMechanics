e1cd080df38efe82c56038d010a2e94dedab66c6
Michal Borowiecki <mbor81@gmail.com>
2016-09-01 21:02:08 +0100

    define commands

---
+++ helloworld-impl/src/main/java/mihbor/lagom/game/impl/GameCommand.java
@@ -0,0 +1,15 @@
+package mihbor.lagom.game.impl;
+
+import com.lightbend.lagom.javadsl.persistence.PersistentEntity;
+
+import mihbor.lagom.game.impl.GameEvent.*;
+
+public interface GameCommand {
+	
+	public final class ProposeGame implements GameCommand, PersistentEntity.ReplyType<GameProposed> {}
+	public final class JoinGame implements GameCommand, PersistentEntity.ReplyType<PlayerJoinedGame> {}
+	public final class StartGame implements GameCommand, PersistentEntity.ReplyType<GameStarted> {}
+	public final class SetPlayerOrder implements GameCommand, PersistentEntity.ReplyType<PlayerOrderSet> {}
+	public final class TakeAction implements GameCommand, PersistentEntity.ReplyType<ActionTaken> {}
+	public final class EndTurn implements GameCommand, PersistentEntity.ReplyType<PlayersTurnEnded> {}
+}
