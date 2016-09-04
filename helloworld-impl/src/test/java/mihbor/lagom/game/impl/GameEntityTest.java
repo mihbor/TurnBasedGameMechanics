7b30b71966f0b110e210a8153744148bfb7d1d2b
Michal Borowiecki <mbor81@gmail.com>
2016-09-04 19:16:02 +0100

    improve first entity test

--- helloworld-impl/src/test/java/mihbor/lagom/game/impl/GameEntityTest.java
+++ helloworld-impl/src/test/java/mihbor/lagom/game/impl/GameEntityTest.java
@@ -1,24 +1,20 @@
 package mihbor.lagom.game.impl;
 
-import static mihbor.lagom.game.impl.GameCommand.*;
 import static org.junit.Assert.*;
 
 import java.util.Collections;
-import java.util.Optional;
 
-import javax.validation.constraints.AssertTrue;
-
-import com.lightbend.lagom.javadsl.persistence.PersistentEntity.InvalidCommandException;
 import com.lightbend.lagom.javadsl.testkit.PersistentEntityTestDriver;
 import com.lightbend.lagom.javadsl.testkit.PersistentEntityTestDriver.Outcome;
 import org.junit.AfterClass;
 import org.junit.BeforeClass;
 import org.junit.Test;
 
-import akka.Done;
 import akka.actor.ActorSystem;
 import akka.testkit.JavaTestKit;
+
 import mihbor.lagom.game.impl.GameEvent.GameProposed;
+import static mihbor.lagom.game.impl.GameCommand.*;
 
 public class GameEntityTest {
 
@@ -37,12 +33,20 @@ public class GameEntityTest {
 
   @Test
   public void testProposeGame() {
-	  PersistentEntityTestDriver<GameCommand, GameEvent, GameState> driver = new PersistentEntityTestDriver<>(
-		  system, new Game(), null);
+	  //Given
+	  PersistentEntityTestDriver<GameCommand, GameEvent, GameState> driver = 
+		  new PersistentEntityTestDriver<>(system, new Game(), null);
+	  
+	  //When
 	  Outcome<GameEvent, GameState> outcome = driver.run(new ProposeGame("abc"));
+	  
+	  //Then
 	  assertEquals(1, outcome.getReplies().size());
-	  assertTrue(outcome.getReplies().iterator().next() instanceof GameProposed);
-	  assertEquals("abc", ((GameProposed)outcome.getReplies().iterator().next()).gameId);
+	  Object reply = outcome.getReplies().iterator().next();
+	  assertTrue(reply instanceof GameProposed);
+	  assertEquals("abc", ((GameProposed)reply).gameId);
+	  assertEquals("abc", outcome.state().gameId);
+//	  assertEquals(Collections.emptyList(), outcome.issues()); there are serializer warnings
   }
   
 }
