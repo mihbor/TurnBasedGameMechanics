6e867a9ed849d40f8fc83b29f03c7f2a0432a5fb
Michal Borowiecki <mbor81@gmail.com>
2016-09-06 21:28:08 +0100

    happy path service test complete

--- helloworld-impl/src/test/java/mihbor/lagom/game/impl/GameServiceTest.java
+++ helloworld-impl/src/test/java/mihbor/lagom/game/impl/GameServiceTest.java
@@ -11,20 +11,20 @@ import mihbor.lagom.game.api.GameService;
 public class GameServiceTest {
 
 	@Test
-	public void test() throws Exception {
+	public void testHappyPath() throws Exception {
 	    withServer(defaultSetup(), server -> {
 	    	GameService service = server.client(GameService.class);
 	    	
 	        service.proposeGame("Abracadabra").invoke().toCompletableFuture().get(5, SECONDS);
 
-//	        service.joinGame("Abracadabra","Alice").invoke().toCompletableFuture().get(5, SECONDS);
-//	        service.joinGame("Abracadabra","Bob").invoke().toCompletableFuture().get(5, SECONDS);
-//
-//	        service.startGame("Abracadabra").invoke().toCompletableFuture().get(5, SECONDS);
-//	        
-//	        service.endTurn("Abracadabra","Alice",0).invoke().toCompletableFuture().get(5, SECONDS);
-//	        service.endTurn("Abracadabra","Bob",1).invoke().toCompletableFuture().get(5, SECONDS);
-//	        service.endTurn("Abracadabra","Alice",2).invoke().toCompletableFuture().get(5, SECONDS);
+	        service.joinGame("Abracadabra","Alice").invoke().toCompletableFuture().get(5, SECONDS);
+	        service.joinGame("Abracadabra","Bob").invoke().toCompletableFuture().get(5, SECONDS);
+
+	        service.startGame("Abracadabra").invoke().toCompletableFuture().get(5, SECONDS);
+	        
+	        service.endTurn("Abracadabra","Alice",0).invoke().toCompletableFuture().get(5, SECONDS);
+	        service.endTurn("Abracadabra","Bob",1).invoke().toCompletableFuture().get(5, SECONDS);
+	        service.endTurn("Abracadabra","Alice",2).invoke().toCompletableFuture().get(5, SECONDS);
 	    });
 	}
 
